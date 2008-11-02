0020e65c76041d86aff5ff835f3f9604d9192db4
KOSEKI Kengo <koseki@gmail.com>
2008-11-03 02:19:57 +0900

    generate gem template using cutagem

---
+++ lib/mocksmtpd.rb
@@ -0,0 +1,12 @@
+require "smtpserver"
+
+class Mocksmtpd
+  VERSION = '0.0.1'
+
+  def initialize(args)
+  end
+
+  def run
+    puts "MOCK"
+  end
+end
---
+++ test/mocksmtpd_test.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+require "test/unit"
+class MocksmtpdTest < Test::Unit::TestCase
+end
